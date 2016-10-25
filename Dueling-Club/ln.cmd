b0cdcb95924776d63d9d970a5d4eb72b9adb45a1
d.lola <d.lola@example.com>
2016-10-25 22:35:41 +0300

    add new resources

--- Dueling-Club/ln.cmd
+++ Dueling-Club/ln.cmd
@@ -3,7 +3,7 @@ chcp 65001
 set resourcePath="target/resource"
 set xmlPath="target/XML"
 if exist %resourcePath% (rmdir %resourcePath%)
-mklink /D %resourcePath% "%CD%/../resources/src/main/resources"
+mklink /D %resourcePath% "%CD%/../resources/res"
 
 if exist %xmlPath% (rmdir %xmlPath%)
 mklink /D %xmlPath% "%CD%/../C-Engine/src/main/resources/XML"
