90d27668e014932db7ceb1d47baa32d59a2a84ee
d.lola <d.lola@example.com>
2016-11-29 01:08:54 +0300

    Merge branch 'master' of github.com:mephistopheles-repo/battlecraft
    # Conflicts:
    #	Dueling-Club/src/main/java/main/libgdx/DC_GDX_GridPanel.java

---
+++ Dueling-Club/src/main/java/main/libgdx/UnitViewOptions.kt
@@ -0,0 +1,13 @@
+package main.libgdx
+
+import com.badlogic.gdx.graphics.Texture
+
+class UnitViewOptions() {
+    var runnable: Runnable? = null
+    var portrateTexture: Texture? = null
+    var directionPointerTexture: Texture? = null
+    var iconTexture: Texture? = null
+    var clockTexture: Texture? = null
+    var directionValue: Int = 0
+    var clockValue: String? = null
+}
