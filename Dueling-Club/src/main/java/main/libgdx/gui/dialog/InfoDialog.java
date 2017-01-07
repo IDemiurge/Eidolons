67e4abc485bd230663795a443cedb2f964872387
Alex Kamen <justmeakk@gmail.com>
2017-01-07 17:07:20 +0600

    InfoDialog layout

--- Dueling-Club/src/main/java/main/libgdx/gui/dialog/InfoDialog.java
+++ Dueling-Club/src/main/java/main/libgdx/gui/dialog/InfoDialog.java
@@ -9,17 +9,18 @@ import main.libgdx.gui.panels.sub.Container;
  * Created by JustMe on 1/5/2017.
  */
 public class InfoDialog extends Dialog {
-
     private   LayoutParser parser;
     Container top;
     Comp portrait;
     Comp portraitBg;
+    Container fxAndAbils;
     Container armor;
     Container armorTraits;
     Container mainWeapon;
     Container offWeapon;
     Container attributes;
     Container dynamicParams;
+    Container points;
     Container mainParams;
     Container params;
     Container resistances;
@@ -29,8 +30,6 @@ public class InfoDialog extends Dialog {
     String mainLayout = "attributes: 0 0; ";
     public final static String bgPath = "";
 
-
-
     public InfoDialog(DC_HeroObj unit) {
         super(bgPath);
 //        armor = new Container( ->{
@@ -38,7 +37,18 @@ public class InfoDialog extends Dialog {
 //        });
 
         parser=new LayoutParser(this);
-        parser.parse(this, mainLayout, attributes  );
+        parser.parse(this,
+         mainLayout,fxAndAbils, attributes, dynamicParams,mainWeapon,description,
+
+         resistances, armor, mainParams,   points, portrait,
+
+         params, offWeapon, lore
+
+
+
+         );
+
+
     }
 
 
