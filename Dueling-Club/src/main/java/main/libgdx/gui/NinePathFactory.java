828d6ea7dcae6a4490b121db08ae1583c595c46b
d.lola <d.lola@example.com>
2017-03-28 00:17:43 +0300

    ninepath fix

---
+++ Dueling-Club/src/main/java/main/libgdx/gui/NinePathFactory.java
@@ -0,0 +1,12 @@
+package main.libgdx.gui;
+
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+
+import static main.libgdx.texture.TextureCache.getOrCreate;
+
+public class NinePathFactory {
+
+    public static NinePatch getTooltip() {
+        return new NinePatch(getOrCreate("UI/components/tooltip_background.png"), 32, 16, 14, 14);
+    }
+}
