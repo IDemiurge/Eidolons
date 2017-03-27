675e08844efe0313bae144dffd1f6b11e168b897
d.lola <d.lola@example.com>
2017-03-28 02:25:37 +0300

    fix weapon pos

--- Dueling-Club/src/main/java/main/libgdx/gui/panels/dc/unitinfo/MainWeaponPanel.java
+++ Dueling-Club/src/main/java/main/libgdx/gui/panels/dc/unitinfo/MainWeaponPanel.java
@@ -2,61 +2,28 @@ package main.libgdx.gui.panels.dc.unitinfo;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import main.libgdx.gui.panels.dc.TabbedPanel;
-import main.libgdx.gui.panels.dc.TablePanel;
-import main.libgdx.gui.panels.dc.ValueContainer;
 import main.libgdx.gui.panels.dc.unitinfo.datasource.MainWeaponDataSource;
 import main.libgdx.texture.TextureCache;
 
-import java.util.List;
-
-public class MainWeaponPanel extends TabbedPanel {
+public class MainWeaponPanel extends WeaponPanel {
     public MainWeaponPanel() {
         super();
         TextureRegion textureRegion = new
                 TextureRegion(TextureCache.getOrCreate("/UI/components/infopanel/main_weapon.png"));
         TextureRegionDrawable drawable = new TextureRegionDrawable(textureRegion);
         setBackground(drawable);
-
-        pad(50, 10, 5, 10);
     }
 
     @Override
     public void updateAct(float delta) {
         clear();
-        MainWeaponDataSource source = (MainWeaponDataSource) getUserObject();
+        MainWeaponDataSource src = (MainWeaponDataSource) getUserObject();
 
-        TablePanel weapon = new TablePanel();
-        final List<ValueContainer> mainWeaponInfo = source.getMainWeaponDetailInfo();
-        final List<ValueContainer> naturalWeaponInfo = source.getNaturalMainWeaponDetailInfo();
+        addWeapons(src.getMainWeapon(), src.getMainWeaponDetailInfo(), "weapon");
 
-        for (ValueContainer valueContainer : mainWeaponInfo) {
-            weapon.addElement(valueContainer).padLeft(5);
-        }
-
-/*        final int sizeDiff = naturalWeaponInfo.size() - mainWeaponInfo.size();
-        if (sizeDiff > 0){
-            for (int i = 0; i < sizeDiff; i++) {
-                addElement(null).expand(0, 0).fill(true);
-            }
-        }*/
-
-
-        weapon.addElement(source.getMainWeapon()).padLeft(5);
-
-        addTab(weapon, "weapon");
-
-        weapon = new TablePanel();
-
-        for (ValueContainer valueContainer : naturalWeaponInfo) {
-            weapon.addElement(valueContainer).padLeft(5);
-        }
-
-
-        weapon.addElement(source.getNaturalMainWeapon()).padLeft(5);
-
-        addTab(weapon, "unarmed");
+        addWeapons(src.getNaturalMainWeapon(), src.getNaturalMainWeaponDetailInfo(), "unarmed");
 
         resetCheckedTab();
     }
+
 }
--- Dueling-Club/src/main/java/main/libgdx/gui/panels/dc/unitinfo/OffWeaponPanel.java
+++ Dueling-Club/src/main/java/main/libgdx/gui/panels/dc/unitinfo/OffWeaponPanel.java
@@ -2,51 +2,25 @@ package main.libgdx.gui.panels.dc.unitinfo;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import main.libgdx.gui.panels.dc.TabbedPanel;
-import main.libgdx.gui.panels.dc.TablePanel;
-import main.libgdx.gui.panels.dc.ValueContainer;
 import main.libgdx.gui.panels.dc.unitinfo.datasource.OffWeaponDataSource;
 import main.libgdx.texture.TextureCache;
 
-import java.util.List;
-
-public class OffWeaponPanel extends TabbedPanel {
+public class OffWeaponPanel extends WeaponPanel {
     public OffWeaponPanel() {
         super();
         TextureRegion textureRegion = new TextureRegion(TextureCache.getOrCreate("/UI/components/infopanel/off_weapon.png"));
         TextureRegionDrawable drawable = new TextureRegionDrawable(textureRegion);
         setBackground(drawable);
-
-        pad(50, 10, 5, 10);
     }
 
     @Override
     public void updateAct(float delta) {
         clear();
-        OffWeaponDataSource source = (OffWeaponDataSource) getUserObject();
+        OffWeaponDataSource src = (OffWeaponDataSource) getUserObject();
 
-        TablePanel weapon = new TablePanel();
-        final List<ValueContainer> mainWeaponInfo = source.getOffWeaponDetailInfo();
-        final List<ValueContainer> naturalWeaponInfo = source.getNaturalOffWeaponDetailInfo();
+        addWeapons(src.getOffWeapon(), src.getOffWeaponDetailInfo(), "weapon");
 
-        for (ValueContainer valueContainer : mainWeaponInfo) {
-            weapon.addElement(valueContainer).padLeft(5);
-        }
-
-        weapon.addElement(source.getOffWeapon()).padLeft(5);
-
-        addTab(weapon, "weapon");
-
-        weapon = new TablePanel();
-
-        for (ValueContainer valueContainer : naturalWeaponInfo) {
-            weapon.addElement(valueContainer).padLeft(5);
-        }
-
-
-        weapon.addElement(source.getNaturalOffWeapon()).padLeft(5);
-
-        addTab(weapon, "unarmed");
+        addWeapons(src.getNaturalOffWeapon(), src.getNaturalOffWeaponDetailInfo(), "unarmed");
 
         resetCheckedTab();
     }
