716e5c48a1f6bc80cecd7bac2121d877b421f3b7
d.lola <d.lola@example.com>
2017-03-29 01:03:11 +0300

    quick panel prototype

--- Dueling-Club/src/main/java/main/libgdx/gui/panels/dc/actionpanel/ActionPanelController.java
+++ Dueling-Club/src/main/java/main/libgdx/gui/panels/dc/actionpanel/ActionPanelController.java
@@ -1,9 +1,7 @@
 package main.libgdx.gui.panels.dc.actionpanel;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import main.libgdx.gui.panels.dc.actionpanel.datasource.QuickSlotsDataSource;
 import main.system.GuiEventManager;
 
@@ -14,7 +12,7 @@ public class ActionPanelController extends Group {
 
     public ActionPanelController() {
         quickSlotPanel = new QuickSlotPanel();
-        quickSlotPanel.setPosition(70, -quickSlotPanel.getHeight());
+        quickSlotPanel.setPosition(70, -64);
         addActor(quickSlotPanel);
         initListeners();
     }
@@ -24,21 +22,18 @@ public class ActionPanelController extends Group {
             final QuickSlotsDataSource source = (QuickSlotsDataSource) obj.get();
             if (source != null) {
                 if (quickSlotPanel.getY() < 0) {
-                    final MoveToAction action = new MoveToAction();
-                    action.setPosition(70, 0);
-                    action.setDuration(2);
-                    action.setInterpolation(Interpolation.exp5);
-                    quickSlotPanel.addAction(action);
+                    quickSlotPanel.setY(0);
                 }
                 quickSlotPanel.setUserObject(source);
             } else {
-                final MoveToAction action = new MoveToAction();
-                action.setPosition(70, -quickSlotPanel.getHeight());
-                action.setDuration(2);
-                action.setInterpolation(Interpolation.exp5);
-                quickSlotPanel.addAction(action);
+                quickSlotPanel.setY(-64);
             }
         });
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
     }
 
     @Override
