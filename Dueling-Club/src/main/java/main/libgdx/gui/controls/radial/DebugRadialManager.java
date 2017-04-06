ac50118e82f501a0ff843ce79c04f9b685cff388
d.lola <d.lola@example.com>
2017-04-06 21:19:23 +0300

    refactoring

--- Dueling-Club/src/main/java/main/libgdx/gui/controls/radial/DebugRadialManager.java
+++ Dueling-Club/src/main/java/main/libgdx/gui/controls/radial/DebugRadialManager.java
@@ -1,17 +1,41 @@
 package main.libgdx.gui.controls.radial;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import main.entity.obj.DC_Obj;
+import main.game.core.game.DC_Game;
+import main.game.core.game.Game;
+import main.swing.generic.components.editors.lists.ListChooser;
+import main.system.auxiliary.EnumMaster;
+import main.test.debug.DebugMaster;
+import main.test.debug.DebugMaster.DEBUG_FUNCTIONS;
+import main.test.debug.DebugMaster.HIDDEN_DEBUG_FUNCTIONS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by JustMe on 12/28/2016.
  */
 public class DebugRadialManager {
-/*
+
+
+    private static TextureRegion defaultTexture;
+
     static {
         DEBUG_CONTROL.FUNC_OTHER.objects = getUnlistedFunctions().toArray();
+        defaultTexture =
+                new TextureRegion(
+                        new Texture(RadialMenu.class.getResource("/data/marble_green.png").getPath())
+                );
     }
 
 
-    public static List<RadialMenu.CreatorNode> getDebugNodes(DC_Obj obj) {
-        List<RadialMenu.CreatorNode> list = new LinkedList<>();
+    public static List<RadialValueContainer> getDebugNodes(DC_Obj obj) {
+        List<RadialValueContainer> list = new LinkedList<>();
 
         Arrays.stream(DEBUG_CONTROL.values()).forEach(c -> {
             if (c.isRoot()) {
@@ -22,31 +46,27 @@ public class DebugRadialManager {
         return list;
     }
 
-    private static RadialMenu.CreatorNode createNodeBranch(Object object, DC_Obj obj) {
-        RadialMenu.CreatorNode node = new RadialMenu.CreatorNode();
-        node.name = StringMaster.getWellFormattedString(object.toString());
+    private static RadialValueContainer createNodeBranch(Object object, DC_Obj obj) {
+        RadialValueContainer node;
+        List<RadialValueContainer> list = new ArrayList<>();
 
-        boolean leaf = true;
         if (object instanceof DEBUG_CONTROL) {
             DEBUG_CONTROL c = (DEBUG_CONTROL) object;
-
             if (c.getChildObjects() != null) {
-                node.childNodes = new LinkedList<>();
-                for (Object o : c.getChildObjects()) {
-                    node.childNodes.add(createNodeBranch(o, obj));
-                    leaf = false;
-                }
+                list = Arrays.stream(c.getChildObjects())
+                        .map(el -> createNodeBranch(el, obj))
+                        .collect(Collectors.toList()
+                        );
             } else if (c.getChildren() != null) {
-                node.childNodes = new LinkedList<>();
-                for (Object o : c.getChildren()) {
-                    node.childNodes.add(createNodeBranch(o, obj));
-                    leaf = false;
-                }
+                list = Arrays.stream(c.getChildren())
+                        .map(el -> createNodeBranch(el, obj))
+                        .collect(Collectors.toList()
+                        );
             }
-
         }
-        if (leaf) {
-            node.action = () -> {
+        if (list.size() != 0) {
+
+            node = new RadialValueContainer(defaultTexture, () -> {
                 DebugMaster.setTarget(obj);
                 try {
                     if (object instanceof DEBUG_CONTROL) {
@@ -57,8 +77,14 @@ public class DebugRadialManager {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            };
+            });
+        } else {
+            node = new RadialValueContainer(defaultTexture, null);
+            node.setChilds(list);
         }
+
+        RadialManager.addSimpleTooltip(node, object.toString());
+
         return node;
     }
 
@@ -79,40 +105,36 @@ public class DebugRadialManager {
     }
 
     public static void handleDebugControl(Object control) {
-        if (control instanceof DebugMaster.DEBUG_FUNCTIONS) {
-            DebugMaster.DEBUG_FUNCTIONS func_control = ((DebugMaster.DEBUG_FUNCTIONS) control);
-            DC_Game.game.getDebugMaster().executeDebugFunctionNewThread(func_control);
+        if (control instanceof DEBUG_FUNCTIONS) {
+            DC_Game.game.getDebugMaster()
+                    .executeDebugFunctionNewThread(((DEBUG_FUNCTIONS) control));
         }
 
         if (control instanceof HIDDEN_DEBUG_FUNCTIONS) {
-            HIDDEN_DEBUG_FUNCTIONS func_control = ((HIDDEN_DEBUG_FUNCTIONS) control);
-            DC_Game.game.getDebugMaster().executeHiddenDebugFunction(func_control);
-
+            DC_Game.game.getDebugMaster()
+                    .executeHiddenDebugFunction(((HIDDEN_DEBUG_FUNCTIONS) control));
         }
     }
 
     public static void handleDebugControl(DEBUG_CONTROL control) {
-        new Thread(new Runnable() {
-            public void run() {
-                switch (control) {
-                    case SET_VALUE:
-                        Game.game.getValueHelper().promptSetValue();
-                        break;
-                    case PICK:
-
-                        DC_Game.game.getDebugMaster().executeDebugFunction(new EnumMaster<DebugMaster.DEBUG_FUNCTIONS>()
-                                .retrieveEnumConst(DebugMaster.DEBUG_FUNCTIONS.class,
-                                        ListChooser.chooseEnum(DebugMaster.DEBUG_FUNCTIONS.class)));
-                        break;
-                    case TYPE:
-                        DC_Game.game.getDebugMaster().promptFunctionToExecute();
-                        break;
-                    case PICK_HIDDEN:
-                        DC_Game.game.getDebugMaster().executeHiddenDebugFunction(new EnumMaster<DebugMaster.HIDDEN_DEBUG_FUNCTIONS>()
-                                .retrieveEnumConst(DebugMaster.HIDDEN_DEBUG_FUNCTIONS.class,
-                                        ListChooser.chooseEnum(DebugMaster.HIDDEN_DEBUG_FUNCTIONS.class)));
-                        break;
-                }
+        new Thread(() -> {
+            switch (control) {
+                case SET_VALUE:
+                    Game.game.getValueHelper().promptSetValue();
+                    break;
+                case PICK:
+                    DC_Game.game.getDebugMaster().executeDebugFunction(new EnumMaster<DEBUG_FUNCTIONS>()
+                            .retrieveEnumConst(DEBUG_FUNCTIONS.class,
+                                    ListChooser.chooseEnum(DEBUG_FUNCTIONS.class)));
+                    break;
+                case TYPE:
+                    DC_Game.game.getDebugMaster().promptFunctionToExecute();
+                    break;
+                case PICK_HIDDEN:
+                    DC_Game.game.getDebugMaster().executeHiddenDebugFunction(new EnumMaster<HIDDEN_DEBUG_FUNCTIONS>()
+                            .retrieveEnumConst(HIDDEN_DEBUG_FUNCTIONS.class,
+                                    ListChooser.chooseEnum(HIDDEN_DEBUG_FUNCTIONS.class)));
+                    break;
             }
         }, "debug radial click handle").start();
     }
@@ -161,7 +183,7 @@ public class DebugRadialManager {
         TYPE(),
         PICK_HIDDEN(),
         FUNCTION(FUNC_STANDARD, FUNC_ADD_BF, FUNC_ADD_NON_BF, FUNC_GLOBAL,
-         FUNC_OTHER, FUNC_TOGGLE,
+                FUNC_OTHER, FUNC_TOGGLE,
                 PICK, TYPE, PICK_HIDDEN) {
             @Override
             public boolean isRoot() {
@@ -196,7 +218,4 @@ public class DebugRadialManager {
             return objects;
         }
     }
-*/
-
-
 }
