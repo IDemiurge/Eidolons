76e15838c55c349c404a16d4f0602826d4722b19
d.lola <d.lola@example.com>
2017-04-06 16:30:53 +0300

    refactoring

--- Dueling-Club/src/main/java/main/libgdx/gui/controls/radial/RadialMenu.java
+++ Dueling-Club/src/main/java/main/libgdx/gui/controls/radial/RadialMenu.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.utils.Align;
 
 import java.util.List;
 
@@ -17,9 +16,9 @@ public class RadialMenu extends Group {
     public RadialMenu() {
         final Texture t = new Texture(RadialMenu.class.getResource("/data/marble_green.png").getPath());
         closeButton = new RadialValueContainer(new TextureRegion(t), () -> RadialMenu.this.setVisible(false));
-        closeButton.setSize(64, 64);
-        closeButton.setImageAlign(Align.bottomLeft);
-        setSize(64, 64);
+        closeButton.setX(-20);
+        setSize(128, 128);
+        debug();
     }
 
     public void init(List<RadialValueContainer> nodes) {
@@ -48,15 +47,14 @@ public class RadialMenu extends Group {
         currentNode.setChildVisible(true);
         updatePosition();
         setVisible(true);
-
+        System.out.println(getX() + " " + getY() + " | " + currentNode.getX() + " " + currentNode.getY());
     }
 
     public void updatePosition() {
-        currentNode.setPosition(-32, -32);
-
         int step = 360 / currentNode.getChilds().size();
         int pos;
-        int r = (int) (getWidth() * 1.5);
+        final double coefficient = currentNode.getChilds().size() > 6 ? 2 : 1.5;
+        int r = (int) (64 * coefficient);
 
         for (int i = 0; i < currentNode.getChilds().size(); i++) {
             pos = i * step;
@@ -76,67 +74,4 @@ public class RadialMenu extends Group {
             }
         }
     }
-
-/*    private List<MenuNode> createChildren(final MenuNode parent, final List<MenuNodeDataSource> creatorNodes) {
-        List<MenuNode> menuNodes = new ArrayList<>();
-        for (final MenuNodeDataSource node : creatorNodes) {
-            final MenuNode menuNode = new MenuNode(node.getCurrent());
-            menuNode.parent = parent;
-            if (node.getChilds().size() == 0) {
-                final Runnable r = node.getCurrent().getClickAction();
-                final RadialMenu menu = this;
-                menuNode.button.bindAction(() -> {
-                    r.run();
-                    menu.setVisible(false);
-                });
-            } else {
-                menuNode.setChildren(createChildren(menuNode, node.getChilds()));
-            }
-            menuNodes.add(menuNode);
-        }
-        return menuNodes;
-    }
-
-    public class MenuNode extends Group {
-        public MenuNode parent = null;
-        public Runnable action = null;
-        private List<MenuNode> childNodes = new ArrayList<>();
-        private ActionValueContainer button;
-
-        public MenuNode(ActionValueContainer button) {
-            this.button = button;
-            button.setPosition(-(button.getPrefWidth() / 2), -(button.getPrefHeight() / 2));
-            addActor(button);
-        }
-
-        public void setChildren(List<MenuNode> childs) {
-            this.childNodes = childs;
-            for (MenuNode child : childs) {
-                addActor(child);
-            }
-
-            updatePosition();
-            setChildVisible(true);
-        }
-
-        protected void updatePosition() {
-            int step = 360 / childNodes.size();
-            int pos;
-            int r = (int) (getWidth() * 1.5);
-
-            for (int i = 0; i < childNodes.size(); i++) {
-                pos = i * step;
-                int y = (int) (r * Math.sin(Math.toRadians(pos + 90)));
-                int x = (int) (r * Math.cos(Math.toRadians(pos + 90)));
-                childNodes.get(i).setPosition(x + currentNode.getX(), y + currentNode.getY());
-            }
-        }
-
-        public void setChildVisible(boolean visible) {
-            childNodes.forEach(el -> el.button.setVisible(visible));
-*//*            if (visible) {
-                updatePosition();
-            }*//*
-        }
-    }*/
 }
