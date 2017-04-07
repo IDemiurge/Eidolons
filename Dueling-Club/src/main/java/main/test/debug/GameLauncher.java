8bb4e65d69fba32777e9fb7779c42761c36ad4f0
Alex Kamen <justmeakk@gmail.com>
2017-04-07 17:56:15 +0700

    added data for profiling

--- Dueling-Club/src/main/java/main/test/debug/GameLauncher.java
+++ Dueling-Club/src/main/java/main/test/debug/GameLauncher.java
@@ -49,7 +49,7 @@ public class GameLauncher {
     // private boolean RANDOMIZE_ENEMIES_PARTY = true;
     public boolean LEADER_MOVES_FIRST = false;
     public String ENEMY_PARTY = "Pirate";
-    public String PLAYER_PARTY = "Warlock";//"Bandit Archer";//Zail Adelwyn v4
+    public String PLAYER_PARTY = "Warlock;Pirate;Pirate;Pirate;Pirate;Pirate;Pirate;Pirate;Pirate;Pirate;Pirate";//"Bandit Archer";//Zail Adelwyn v4
     public boolean DUMMY_MODE = false;
     public boolean DUMMY_PP = false;
     public Boolean FAST_MODE;
