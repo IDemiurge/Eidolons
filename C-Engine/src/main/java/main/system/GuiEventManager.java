1f5487df492af1dd002b43981a74f972ea98cf1e
d.lola <d.lola@example.com>
2017-05-24 00:20:59 +0300

    some events changes

--- C-Engine/src/main/java/main/system/GuiEventManager.java
+++ C-Engine/src/main/java/main/system/GuiEventManager.java
@@ -1,10 +1,9 @@
 package main.system;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -20,14 +19,24 @@ public class GuiEventManager {
     private Map<GuiEventType, EventCallback> eventMap = new HashMap<>();
     private List<Runnable> eventQueue = new ArrayList<>();
     private Lock lock = new ReentrantLock();
-    private Map<GuiEventType, OnDemandCallback> onDemand = new ConcurrentHashMap<>();
+    private Map<GuiEventType, List<EventCallback>> onceBinds = new HashMap<>();
 
     public static void bind(GuiEventType type, final EventCallback event) {
         getInstance().bind_(type, event);
     }
 
+    public static void once(GuiEventType type, final EventCallback event) {
+        getInstance().once_(type, event);
+    }
+
+    private void once_(GuiEventType type, EventCallback event) {
+        onceBinds
+                .getOrDefault(type, new LinkedList<>())
+                .add(event);
+    }
+
     public static void trigger(final GuiEventType type) {
-        trigger(type, new EventCallbackParam<>(null));
+        trigger(type, new EventCallbackParam(null));
     }
 
     public static void trigger(final GuiEventType type, Object obj) {
@@ -36,7 +45,7 @@ public class GuiEventManager {
         if (obj instanceof EventCallbackParam) {
             eventCallback = (EventCallbackParam) obj;
         } else {
-            eventCallback = new EventCallbackParam<>(obj);
+            eventCallback = new EventCallbackParam(obj);
         }
 
         getInstance().trigger_(type, eventCallback);
@@ -66,17 +75,6 @@ public class GuiEventManager {
 
     public void bind_(GuiEventType type, final EventCallback event) {
         if (event != null) {
-            if (onDemand.containsKey(type)) {
-                lock.lock();
-                OnDemandCallback r = onDemand.remove(type);
-                try {
-                    eventQueue.add(() -> r.call(event));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    lock.unlock();
-                }
-            }
             if (eventMap.containsKey(type)) {
                 final EventCallback old = eventMap.remove(type);
                 eventMap.put(type, (obj) -> {
@@ -93,26 +91,28 @@ public class GuiEventManager {
         }
     }
 
-    public void trigger_(final GuiEventType type, final EventCallbackParam obj) {
+    List<Pair<GuiEventType, EventCallbackParam>> triggerList = new LinkedList<>();
 
-        if (eventMap.containsKey(type)) {
-            lock.lock();
-            try {
-                eventQueue.add(() -> eventMap.get(type).call(obj));
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
-        } else {
-            if (obj instanceof OnDemandEventCallbackParam) {
-                onDemand.put(type, (callback) -> callback.call(obj));
-            }
-        }
+    public void trigger_(final GuiEventType type, final EventCallbackParam obj) {
+        triggerList.add(new ImmutablePair<>(type, obj));
     }
 
     public void processEvents_() {
-        if (eventQueue.size() > 0) {
+        if (onceBinds.size() > 0) {
+            lock.lock();
+            final Map<GuiEventType, List<EventCallback>> map = onceBinds;
+            triggerList.forEach(pair -> {
+                final List<EventCallback> callbacks = map.get(pair.getKey());
+                if (callbacks != null) {
+                    callbacks.forEach(
+                            eventCallback -> eventCallback.call(pair.getValue())
+                    );
+                    map.remove(pair.getKey());
+                }
+            });
+        }
+
+        if (triggerList.size() > 0) {
             lock.lock();
             List<Runnable> list = eventQueue;
             eventQueue = new ArrayList<>();
@@ -121,5 +121,4 @@ public class GuiEventManager {
             list.forEach(Runnable::run);
         }
     }
-
 }
