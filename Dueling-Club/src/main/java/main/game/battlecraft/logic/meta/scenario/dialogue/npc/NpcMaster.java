76be266c7235c64fde61999b2bf16b5ecaa75e82
Alex Kamen <justmeakk@gmail.com>
2017-05-30 19:46:07 +0700

    sync

---
+++ Dueling-Club/src/main/java/main/game/battlecraft/logic/meta/scenario/dialogue/npc/NpcMaster.java
@@ -0,0 +1,21 @@
+package main.game.battlecraft.logic.meta.scenario.dialogue.npc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by JustMe on 5/27/2017.
+ * In-battle comments
+ Managing Dialogues - availability,
+ Advanced Dialogue Syntax
+ *
+ *
+ */
+public class NpcMaster {
+
+    public void getDisplayedUnitComments(){
+
+         List<BattleComment> list = new LinkedList<>();
+    }
+
+}
