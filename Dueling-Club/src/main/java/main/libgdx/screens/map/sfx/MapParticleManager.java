a3dfa7e547669169e44efdeacbdcad891bcf55f3
Alex Kamen <justmeakk@gmail.com>
2018-02-18 09:32:08 +0600

    sync commit

---
+++ Dueling-Club/src/main/java/main/libgdx/screens/map/sfx/MapParticleManager.java
@@ -0,0 +1,13 @@
+package main.libgdx.screens.map.sfx;
+
+import main.libgdx.anims.particles.EmitterActor;
+
+/**
+ * Created by JustMe on 2/17/2018.
+ */
+public class MapParticleManager {
+
+    EmitterActor actor;
+
+
+}
---
+++ Dueling-Club/src/main/java/main/libgdx/screens/map/sfx/MapShader.java
@@ -0,0 +1,7 @@
+package main.libgdx.screens.map.sfx;
+
+/**
+ * Created by JustMe on 2/18/2018.
+ */
+public class MapShader {
+}
