8117933253065b82b55c6c75a01bec9d5a0c747c
Ilya <ikamenshchikov@gmail.com>
2018-06-08 21:40:30 +0200

    new macro skeleton

---
+++ Dueling-Club/src/main/java/eidolons/game/macro/Party.java
@@ -0,0 +1,18 @@
+package eidolons.game.macro;
+
+import eidolons.entity.obj.unit.Unit;
+
+/**
+ * Created by Giskard on 6/8/2018.
+ */
+public class Party {
+
+    private Unit hero;
+    private int x;
+    private int y;
+
+    Party(Unit hero){
+        this.hero = hero;
+    }
+
+}
