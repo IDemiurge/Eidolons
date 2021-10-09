6aa0b09b39d75155f9d9f80e2569d2d2860efd84
IDemiurge <alexanderkkamen@gmail.com>
2021-10-09 15:39:15 +0300

    Math tests templates added

---
+++ DungeonCraft/src/test/java/framework/TestMathUtils.java
@@ -0,0 +1,10 @@
+package framework;
+
+import main.content.enums.GenericEnums;
+
+public class TestMathUtils {
+
+    public static int dieRoll(Boolean min_max_average, Integer dice, GenericEnums.DieType diceType) {
+        return 0;
+    }
+}
