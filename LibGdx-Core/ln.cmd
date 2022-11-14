7f9c9e09f8528e906dfa6d7778a9e7bade001d58
IDemiurge <alexanderkkamen@gmail.com>
2022-11-14 19:43:18 +0100

    updated ln.cmd for new resources folder

--- LibGdx-Core/ln.cmd
+++ LibGdx-Core/ln.cmd
@@ -31,7 +31,7 @@ chcp 65001
 set resourcePath="target/resources"
 set xmlPath="target/XML"
 if exist %resourcePath% (rmdir %resourcePath%)
-mklink /D %resourcePath% "%CD%/../resources/res"
+mklink /D %resourcePath% "%CD%/../resources/common"
 
 if exist %xmlPath% (rmdir %xmlPath%)
 mklink /D %xmlPath% "%CD%/../C-Engine/src/main/resources/XML"
